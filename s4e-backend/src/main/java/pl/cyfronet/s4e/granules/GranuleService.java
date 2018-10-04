771c753375855d61385a843a472216db33576739
Jakub Sawicki <j.sawicki@cyfronet.pl>
2018-10-04 15:48:01 +0200

    Update granules returned from backend

--- s4e-backend/src/main/java/pl/cyfronet/s4e/granules/GranuleService.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/granules/GranuleService.java
@@ -1,62 +1,33 @@
 package pl.cyfronet.s4e.granules;
 
+import lombok.val;
 import org.springframework.stereotype.Service;
 import pl.cyfronet.s4e.ex.NotFoundException;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class GranuleService {
-    private static final LocalDateTime BASE_TIME = LocalDateTime.of(2018, 10, 2, 10, 0);
-    private static final String LAYER_NAME = "test:201807051330_PL_HRV_gtif_mercator";
+    private static final LocalDateTime BASE_TIME = LocalDateTime.of(2018, 10, 4, 8, 0);
 
     public List<Granule> getGranules(Long productId) throws NotFoundException {
-        if (productId == 1L) {
-            return Arrays.asList(new Granule[] {
-                    Granule.builder()
-                            .id(1L)
-                            .productId(productId)
-                            .timestamp(BASE_TIME)
-                            .layerName(LAYER_NAME)
-                            .build(),
-                    Granule.builder()
-                            .id(2L)
-                            .productId(productId)
-                            .timestamp(BASE_TIME.minusHours(1))
-                            .layerName(LAYER_NAME)
-                            .build(),
-                    Granule.builder()
-                            .id(3L)
-                            .productId(productId)
-                            .timestamp(BASE_TIME.minusHours(2))
-                            .layerName(LAYER_NAME)
-                            .build(),
-            });
-        } else if (productId == 2L) {
-            return Arrays.asList(new Granule[] {
-                    Granule.builder()
-                            .id(4L)
-                            .productId(productId)
-                            .timestamp(BASE_TIME)
-                            .layerName(LAYER_NAME)
-                            .build(),
-                    Granule.builder()
-                            .id(5L)
-                            .productId(productId)
-                            .timestamp(BASE_TIME.minusHours(1))
-                            .layerName(LAYER_NAME)
-                            .build(),
-                    Granule.builder()
-                            .id(6L)
-                            .productId(productId)
-                            .timestamp(BASE_TIME.minusHours(2))
-                            .layerName(LAYER_NAME)
-                            .build(),
-            });
-        } else {
+        if (productId != 1L && productId != 2L) {
             throw new NotFoundException();
         }
+        val out = new ArrayList<Granule>();
+        val count = 4;
+        for (int i = 3; i >= 0; i--) {
+            LocalDateTime timestamp = BASE_TIME.plusMinutes(15*i);
+            out.add(Granule.builder()
+                    .id(productId*count+i)
+                    .productId(productId)
+                    .timestamp(timestamp)
+                    .layerName("test:"+ DateTimeFormatter.ofPattern("yyyyMMddHHmm").format(timestamp) +"_Merkator_Europa_ir_108m")
+                    .build());
+        }
+        return out;
     }
 }
