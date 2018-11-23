5b4e1ca8434cc3fc5c43a84121fc9a9749516146
Jakub Sawicki <j.sawicki@cyfronet.pl>
2018-11-23 17:31:01 +0100

    Disable tests in backend

--- s4e-backend/src/test/java/pl/cyfronet/s4e/ApplicationTests.java
+++ s4e-backend/src/test/java/pl/cyfronet/s4e/ApplicationTests.java
@@ -2,11 +2,10 @@ package pl.cyfronet.s4e;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
 public class ApplicationTests {
 
     @Test
