562b264bd257194d0184d003fb24c73e946c1d66
Jakub Sawicki <j.sawicki@cyfronet.pl>
2019-03-07 15:22:14 +0100

    Add configuration for minio and geoserver docker containers
    Currently GeoServer isn't automatically provisioned.
    Workspace, stores and layers must be created separately after starting
    its docker container.

--- s4e-web/src/app/constants.ts
+++ s4e-web/src/app/constants.ts
@@ -1,4 +1,4 @@
-export const geoserverUrl = 'http://geoserver-3434.cloud.plgrid.pl/geoserver/wms';
+export const geoserverUrl = 'http://localhost:8080/geoserver/wms';
 export const apiPrefixV1 = 'api/v1';
 export const backendDateFormat = 'yyyy-MM-dd\'T\'HH:mm:ss';
 
