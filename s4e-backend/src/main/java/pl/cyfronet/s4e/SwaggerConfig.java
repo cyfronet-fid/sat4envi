e4485b6eac2662e7bad71aca1f2223c8c7c0a2ea
Jakub Sawicki <j.sawicki@cyfronet.pl>
2019-04-05 16:40:04 +0200

    API documentation with Swagger

---
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package pl.cyfronet.s4e;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                    .apis(RequestHandlerSelectors.basePackage("pl.cyfronet.s4e.controller"))
+                    .paths(PathSelectors.ant("/api/v1*/**"))
+                    .build()
+                .groupName("v1")
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sat4Envi backend API")
+                .description("The API documentation of the backend.")
+                .version("v1")
+                .build();
+    }
+}
