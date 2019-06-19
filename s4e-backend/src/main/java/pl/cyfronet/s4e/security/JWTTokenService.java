0ab9b37b32f1f38793da7aadc4fbef1dd37c2db4
Jakub Sawicki <j.sawicki@cyfronet.pl>
2019-06-19 14:39:50 +0200

    [#52] Include authorities in JWS claims
    Modify signature of JWTTokenService::generateClaimsJws, which accepts
    Authentication object now, which is necessary to extract authorities.

--- s4e-backend/src/main/java/pl/cyfronet/s4e/security/JWTTokenService.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/security/JWTTokenService.java
@@ -7,10 +7,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.JacksonDeserializer;
 import io.jsonwebtoken.io.JacksonSerializer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 import static pl.cyfronet.s4e.SecurityConfig.JWT_KEY;
 
@@ -18,6 +20,7 @@ import static pl.cyfronet.s4e.SecurityConfig.JWT_KEY;
 @RequiredArgsConstructor
 public class JWTTokenService {
     private static final long EXPIRATION_TIME = Duration.ofHours(1).toMillis();
+    protected static final String AUTHORITIES_KEY = "authorities";
 
     private final ObjectMapper objectMapper;
 
@@ -28,10 +31,15 @@ public class JWTTokenService {
                 .parseClaimsJws(token);
     }
 
-    public String generateClaimsJws(String subject) {
+    public String generateClaimsJws(Authentication auth) {
+        Claims claims = Jwts.claims()
+                .setSubject(auth.getName())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME));
+        claims.put(AUTHORITIES_KEY, auth.getAuthorities().stream()
+                .map(ga -> ga.getAuthority())
+                .collect(Collectors.toList()));
         return Jwts.builder()
-                .setSubject(subject)
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .setClaims(claims)
                 .signWith(JWT_KEY)
                 .serializeToJsonWith(new JacksonSerializer<>(objectMapper))
                 .compact();
