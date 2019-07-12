321ade831ea32b81575d36c597bdf30a68ab0b71
kmarszalek <marszalekk@gmail.com>
2019-07-12 14:44:34 +0200

    #31 Password change

--- s4e-backend/src/main/java/pl/cyfronet/s4e/controller/PasswordController.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/controller/PasswordController.java
@@ -8,6 +8,9 @@ import lombok.val;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pl.cyfronet.s4e.bean.AppUser;
 import pl.cyfronet.s4e.controller.request.PasswordResetRequest;
@@ -15,6 +18,7 @@ import pl.cyfronet.s4e.event.OnPasswordResetTokenEmailEvent;
 import pl.cyfronet.s4e.ex.BadRequestException;
 import pl.cyfronet.s4e.ex.NotFoundException;
 import pl.cyfronet.s4e.ex.PasswordResetTokenExpiredException;
+import pl.cyfronet.s4e.security.AppUserDetails;
 import pl.cyfronet.s4e.service.AppUserService;
 import pl.cyfronet.s4e.service.PasswordService;
 
@@ -33,6 +37,7 @@ public class PasswordController {
     private final AppUserService appUserService;
     private final PasswordService passwordService;
     private final ApplicationEventPublisher eventPublisher;
+    private final PasswordEncoder passwordEncoder;
 
     @ApiOperation("Create and send a password reset token based on email")
     @ApiResponses({
@@ -68,7 +73,7 @@ public class PasswordController {
     @ApiOperation("Reset password")
     @ApiResponses({
             @ApiResponse(code = 200, message = "Password reset was successful"),
-            @ApiResponse(code = 404, message = "Passwords were incorrect"),
+            @ApiResponse(code = 400, message = "Passwords were incorrect"),
             @ApiResponse(code = 404, message = "The token was not found")
     })
     @PostMapping(value = "/password-reset")
@@ -76,11 +81,34 @@ public class PasswordController {
     public ResponseEntity<?> resetPassword(@RequestBody @Valid PasswordResetRequest passwordReset, @RequestParam("token") String token) throws NotFoundException, BadRequestException {
         val optionalResetPasswordToken = passwordService.findByToken(token)
                 .orElseThrow(() -> new NotFoundException("Provided token '" + token + "' not found"));
-        if (optionalResetPasswordToken.getAppUser().getPassword().equals(passwordReset.getOldPassword())) {
+        if (optionalResetPasswordToken.getAppUser().getPassword().equals(passwordEncoder.encode(passwordReset.getOldPassword()))) {
             AppUser user = optionalResetPasswordToken.getAppUser();
-            user.setPassword(passwordReset.getNewPassword());
+            user.setPassword(passwordEncoder.encode(passwordReset.getNewPassword()));
             appUserService.update(user);
             passwordService.delete(optionalResetPasswordToken.getId());
+        } else {
+            throw new BadRequestException("Provided passwords were incorrect");
+        }
+
+        return ResponseEntity.ok().build();
+    }
+
+    @ApiOperation("Change password")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Password change was successful"),
+            @ApiResponse(code = 400, message = "Passwords were incorrect"),
+            @ApiResponse(code = 404, message = "The user was not found")
+    })
+    @PostMapping(value = "/password-change")
+    @ResponseBody
+    @PreAuthorize("!isAnonymous()")
+    public ResponseEntity<?> changePassword(@RequestBody @Valid PasswordResetRequest passwordReset) throws NotFoundException, BadRequestException {
+        val optionalAppUser = appUserService.findByEmail(
+                ((AppUserDetails) SecurityContextHolder.getContext().getAuthentication().getDetails()).getAppUser().getEmail())
+                .orElseThrow(() -> new NotFoundException());
+        if (optionalAppUser.getPassword().equals(passwordEncoder.encode(passwordReset.getOldPassword()))) {
+            optionalAppUser.setPassword(passwordEncoder.encode(passwordReset.getNewPassword()));
+            appUserService.update(optionalAppUser);
         } else {
             throw new BadRequestException("Provided passwords were incorrect");
         }
