a2cfb15ca95c37acd64db0189096343706c92424
Michał Szostak <szostak.m.f@gmail.com>
2019-12-03 15:40:36 +0100

    [#254] Drop Down for ZK
    * Add dropdown for ZK to MapViewComponent
    * Add tests to MapViewComponent
    * Change z-index of modals, to make them always on top
    
    Closes #254

---
+++ s4e-web/src/app/views/map-view/map-view.component.spec.ts
@@ -0,0 +1,64 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {MapViewComponent} from './map-view.component';
+import {MapModule} from './map.module';
+import {TestingConfigProvider} from '../../app.configuration.spec';
+import {RouterTestingModule} from '@angular/router/testing';
+import {By} from '@angular/platform-browser';
+import {MapService} from './state/map/map.service';
+import {SessionStore} from '../../state/session/session.store';
+
+describe('MapViewComponent', () => {
+  let component: MapViewComponent;
+  let fixture: ComponentFixture<MapViewComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [MapModule, RouterTestingModule],
+      providers: [TestingConfigProvider]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MapViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('ZK Options', () => {
+    it('clicking #zk-options-button should show options', () => {
+      // log in user
+      (TestBed.get(SessionStore) as SessionStore).update({token: 'valid'});
+      fixture.detectChanges();
+      fixture.debugElement.query(By.css('#zk-options-button')).nativeElement.click();
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('.zk-options'))).toBeTruthy();
+    });
+
+    it('clicking .backdrop should toggleZKOptions(false)', () => {
+      // log in user
+      (TestBed.get(SessionStore) as SessionStore).update({token: 'valid'});
+      fixture.detectChanges();
+      fixture.debugElement.query(By.css('#zk-options-button')).nativeElement.click();
+      fixture.detectChanges();
+      const spy = spyOn(component, 'toggleZKOptions');
+      fixture.debugElement.query(By.css('.backdrop')).nativeElement.click();
+      expect(spy).toHaveBeenCalledWith(false);
+    });
+
+    it('toggleZKOptions should call mapService.toggleZKOptions ', () => {
+      const service: MapService = TestBed.get(MapService);
+      const spy = spyOn(service, 'toggleZKOptions');
+      component.toggleZKOptions();
+      expect(spy).toHaveBeenCalledWith(true);
+    });
+
+    it('should not show zk-options-button if user is not logged in', () => {
+      expect(fixture.debugElement.query(By.css('#zk-options-button'))).toBeFalsy();
+    });
+  });
+});
