b3630748459a4d9b7c0db35a5408728e3297ab2a
Jakub Sawicki <j.sawicki@cyfronet.pl>
2019-12-10 11:14:37 +0100

    [#267] Remove OpenAPIConfig and merge annotation with Application

--- s4e-backend/src/main/java/pl/cyfronet/s4e/Application.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/Application.java
@@ -1,8 +1,17 @@
 package pl.cyfronet.s4e;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Sat4Envi backend API",
+                description = "The API documentation of the backend.",
+                version = "v1"
+        )
+)
 @SpringBootApplication
 public class Application {
 
