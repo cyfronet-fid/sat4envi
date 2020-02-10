57f954b422bb2cb995e10e2c36d618e255178cc9
michal-szostak <szostak.m.f@gmail.com>
2020-02-10 17:01:51 +0100

    [#345] Generate PDF Report according to the blueprint
    * Add PDF generation
    * Add e2e tests to check whether report is being generated, with PDF artifact uploaded for human
      verification
    
    Closes: #345

--- s4e-web/cypress/plugins/index.js
+++ s4e-web/cypress/plugins/index.js
@@ -1,5 +1,37 @@
 const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
+const fs = require('fs');
 
-module.exports = on => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor)
+module.exports = (on, config) => {
+  on('file:preprocessor', cypressTypeScriptPreprocessor);
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+  });
+  on('task', {
+    compareDownloadReport(now, timeout = config.defaultCommandTimeout) {
+      now = now.replace(/:/g, '_');
+      const downloadsFolder = require('downloads-folder');
+      const path = downloadsFolder();
+      const expectedFilename = path + '/' + `RAPORT.${now}.pdf`;
+      const promise = new Promise((resolve, reject) => {
+        let interval = setInterval(() => {
+          if (fs.existsSync(expectedFilename)) {
+            if (config.env['DONT_DELETE_DOWNLOADED_FILES'] !== true) {
+              fs.unlinkSync(expectedFilename);
+            }
+            clearInterval(interval);
+            interval = null;
+            resolve(true);
+          }
+        }, 500);
+
+        setTimeout(() => {
+          if (interval) {
+            clearInterval(interval);
+            resolve(false);
+          }
+        }, timeout);
+      });
+
+      return promise
+    }
+  });
 };
--- s4e-web/src/app/views/map-view/zk/report-modal/report-generator.spec.ts
+++ s4e-web/src/app/views/map-view/zk/report-modal/report-generator.spec.ts
@@ -1,3 +1,39 @@
-describe('ReportGenerator', () => {
+import {ReportGenerator} from "./report-generator";
+import {TestBed} from "@angular/core/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {HttpClient} from "@angular/common/http";
+import {take, toArray} from "rxjs/operators";
+import {of} from "rxjs";
 
+describe('ReportGenerator', () => {
+  let generator: ReportGenerator;
+  let http: HttpClient;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, ]
+    });
+    http = TestBed.get(HttpClient);
+    generator = new ReportGenerator(http, '', 1, 1)
+  });
+
+  it('working$ and loading$ should be set properly', async function () {
+    expect(await generator.loading$.pipe(take(1)).toPromise()).toBe(true);
+    expect(await generator.working$.pipe(take(1)).toPromise()).toBe(false);
+  });
+
+  it('loadAssets should load fonts and images', async function () {
+    let s1 = spyOn((generator as any), 'loadFont').and.returnValue(of(true).toPromise());
+    let s2 = spyOn((generator as any), 'loadImage').and.returnValue(of(true).toPromise());
+
+    generator.loadAssets();
+
+    expect(s1).toHaveBeenCalledTimes(2);
+    // expect(s1.calls[0]).toEqual(import('./fonts/Ubuntu-Regular-normal'));
+    // expect(s1.calls[1]).toEqual(import('./fonts/Ubuntu-Regular-bold'));
+    expect(s2).toHaveBeenCalledTimes(2);
+    // expect(s2.calls[0]).toEqual('asd');
+    // expect(s2.calls[1]).toEqual('asd');
+    expect(await generator.loading$.pipe(take(2), toArray()).toPromise()).toEqual([true, false]);
+  });
 });
