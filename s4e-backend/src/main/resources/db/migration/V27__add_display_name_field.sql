8ad67f254f41f0b87fa5d5327926be0eb9660e05
Jakub Sawicki <j.sawicki@cyfronet.pl>
2020-02-14 13:16:37 +0100

    Fix migration 27 to migrate data into Product.displayName column

--- s4e-backend/src/main/resources/db/migration/V27__add_display_name_field.sql
+++ s4e-backend/src/main/resources/db/migration/V27__add_display_name_field.sql
@@ -1,2 +1,5 @@
-ALTER TABLE product ADD COLUMN
-    display_name VARCHAR UNIQUE NOT NULL;
+ALTER TABLE product ADD COLUMN display_name VARCHAR UNIQUE;
+
+UPDATE product SET display_name = name;
+
+ALTER TABLE product ALTER COLUMN display_name SET NOT NULL;
