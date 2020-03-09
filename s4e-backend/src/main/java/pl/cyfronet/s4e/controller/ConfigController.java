82fa5c20b605c6aba1990ad0461a601d751df63b
Jakub Sawicki <j.sawicki@cyfronet.pl>
2020-03-09 16:48:02 +0100

    [#381] Return site-key instead of secret-key in ConfigController

--- s4e-backend/src/main/java/pl/cyfronet/s4e/controller/ConfigController.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/controller/ConfigController.java
@@ -1,11 +1,11 @@
 package pl.cyfronet.s4e.controller;
 
-import com.github.mkopylec.recaptcha.RecaptchaProperties;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,9 @@ import static pl.cyfronet.s4e.Constants.API_PREFIX_V1;
 @Tag(name = "config", description = "The Config API")
 public class ConfigController {
     private final GeoServerProperties geoServerProperties;
-    private final RecaptchaProperties recaptchaProperties;
+
+    @Value("${recaptcha.validation.siteKey}")
+    private String recaptchaSiteKey;
 
     @Operation(summary = "Get the front-end app configuration")
     @ApiResponses({
@@ -32,7 +34,7 @@ public class ConfigController {
         return ConfigResponse.builder()
                 .geoserverUrl(geoServerProperties.getOutsideBaseUrl())
                 .geoserverWorkspace(geoServerProperties.getWorkspace())
-                .recaptchaSiteKey(recaptchaProperties.getValidation().getSecretKey())
+                .recaptchaSiteKey(recaptchaSiteKey)
                 .build();
     }
 }
