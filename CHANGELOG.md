9f73442107eb63b88ac6e4ebbcbd4258dd8e8d2c
Jakub Sawicki <j.sawicki@cyfronet.pl>
2020-03-09 16:48:02 +0100

    [#381] Return site-key instead of secret-key in ConfigController

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -25,6 +25,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+- Return site-key instead of secret-key in ConfigController [#381](https://github.com/cyfronet-fid/sat4envi/issues/381)
 - Build in private scope [#372](https://github.com/cyfronet-fid/sat4envi/pull/372)
 - Constraints names on Product and Scene tables [#364](https://github.com/cyfronet-fid/sat4envi/pull/364)
 
--- s4e-backend/src/test/java/pl/cyfronet/s4e/controller/ConfigControllerTest.java
+++ s4e-backend/src/test/java/pl/cyfronet/s4e/controller/ConfigControllerTest.java
@@ -2,6 +2,7 @@ package pl.cyfronet.s4e.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.cyfronet.s4e.BasicTest;
@@ -18,6 +19,9 @@ public class ConfigControllerTest {
     @Autowired
     private GeoServerProperties geoServerProperties;
 
+    @Value("${recaptcha.validation.siteKey}")
+    private String recaptchaSiteKey;
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -25,6 +29,7 @@ public class ConfigControllerTest {
     public void shouldReturnConfiguration() throws Exception {
         mockMvc.perform(get(API_PREFIX_V1 + "/config"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.geoserverUrl").value(geoServerProperties.getOutsideBaseUrl()));
+                .andExpect(jsonPath("$.geoserverUrl").value(geoServerProperties.getOutsideBaseUrl()))
+                .andExpect(jsonPath("$.recaptchaSiteKey").value(recaptchaSiteKey));
     }
 }
