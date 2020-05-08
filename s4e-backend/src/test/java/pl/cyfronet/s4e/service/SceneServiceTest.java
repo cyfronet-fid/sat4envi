3c8e64d26f735cf736a07f69b35d6dcbca0428c3
Jakub Sawicki <j.sawicki@cyfronet.pl>
2020-05-08 11:34:20 +0200

    [#440] Remove outdated webhook handling

--- s4e-backend/src/test/java/pl/cyfronet/s4e/service/SceneServiceTest.java
+++ s4e-backend/src/test/java/pl/cyfronet/s4e/service/SceneServiceTest.java
@@ -4,19 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.cyfronet.s4e.BasicTest;
 import pl.cyfronet.s4e.TestGeometryHelper;
-import pl.cyfronet.s4e.bean.Scene;
 import pl.cyfronet.s4e.bean.Product;
-import pl.cyfronet.s4e.controller.request.WebhookRequest;
-import pl.cyfronet.s4e.data.repository.SceneRepository;
+import pl.cyfronet.s4e.bean.Scene;
 import pl.cyfronet.s4e.data.repository.ProductRepository;
-import pl.cyfronet.s4e.ex.NotFoundException;
-import pl.cyfronet.s4e.properties.GeoServerProperties;
-import pl.cyfronet.s4e.properties.S3Properties;
-import pl.cyfronet.s4e.util.S3Util;
+import pl.cyfronet.s4e.data.repository.SceneRepository;
 import pl.cyfronet.s4e.util.TimeHelper;
 
 import java.time.LocalDateTime;
@@ -25,13 +19,10 @@ import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 @BasicTest
 @Slf4j
 public class SceneServiceTest {
-
     @Autowired
     private ProductRepository productRepository;
 
@@ -42,24 +33,13 @@ public class SceneServiceTest {
     private TimeHelper timeHelper;
 
     @Autowired
-    private S3Properties s3Properties;
-
-    @Autowired
-    private GeoServerProperties geoServerProperties;
-
-    @Autowired
     private TestGeometryHelper geom;
 
     private SceneService sceneService;
 
-    @Mock
-    private S3Util s3Util;
-
-    private static String WEBHOOK_KEY = "s4e-test-1/test/201810042345_Merkator_WV-IR.tif";
-
     @BeforeEach
     public void setUp() {
-        sceneService = new SceneService(sceneRepository, productRepository, timeHelper, s3Util, s3Properties, geoServerProperties);
+        sceneService = new SceneService(sceneRepository, timeHelper);
         sceneRepository.deleteAll();
         productRepository.deleteAll();
     }
@@ -125,37 +105,6 @@ public class SceneServiceTest {
         sceneService.saveScene(scene);
 
         assertThat(sceneRepository.count(), is(equalTo(1L)));
-    }
-
-    @Test
-    public void shouldBuildFromWebhook() throws NotFoundException {
-        WebhookRequest webhookRequest = WebhookRequest.builder()
-                .eventName("EventName")
-                .key(WEBHOOK_KEY)
-                .build();
-        Product product = Product.builder()
-                .name("WV_IR")
-                .displayName("WV-IR")
-                .layerName("test")
-                .build();
-        productRepository.save(product);
-        when(s3Util.getProduct(anyString())).thenReturn("WV_IR");
-
-        Scene buildFromWebhook = sceneService.buildFromWebhook(webhookRequest);
-        assertThat(buildFromWebhook.getProduct().getName(), is(equalTo("WV_IR")));
-    }
-
-    @Test
-    public void shouldReturnProduct() throws NotFoundException {
-        Product product = Product.builder()
-                .name("WV_IR")
-                .displayName("WV-IR")
-                .layerName("test")
-                .build();
-        productRepository.save(product);
-        when(s3Util.getProduct(anyString())).thenReturn("WV_IR");
-        Product fromDb = sceneService.getProduct(WEBHOOK_KEY);
-        assertThat(fromDb.getName(), is(equalTo("WV_IR")));
     }
 
 }
