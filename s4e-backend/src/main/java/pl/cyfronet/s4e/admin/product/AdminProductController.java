46de764b61e39657f8960ce71541b7cca0bd490b
Jakub Sawicki <j.sawicki@cyfronet.pl>
2020-08-06 10:48:55 +0200

    [#620] Add API description to AdminProductController
    The description was missed when creating the controller.

--- s4e-backend/src/main/java/pl/cyfronet/s4e/admin/product/AdminProductController.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/admin/product/AdminProductController.java
@@ -1,5 +1,9 @@
 package pl.cyfronet.s4e.admin.product;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +26,13 @@ public class AdminProductController {
     private final ProductService productService;
     private final AdminProductMapper adminProductMapper;
 
+    @Operation(summary = "Create Product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Incorrect request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+    })
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
     public AdminProductResponse create(@RequestBody @Valid AdminCreateProductRequest request) throws ProductException {
         ProductService.DTO dto = adminProductMapper.toProductServiceDTO(request);
@@ -29,17 +40,38 @@ public class AdminProductController {
         return productService.findByIdFetchSchemas(newId, AdminProductResponse.class).get();
     }
 
+    @Operation(summary = "List Products")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+    })
     @GetMapping
     public List<AdminProductResponse> list() {
         return productService.findAllFetchSchemas(AdminProductResponse.class);
     }
 
+    @Operation(summary = "Return Product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product doesn't exist", content = @Content)
+    })
     @GetMapping("/{id}")
     public AdminProductResponse read(@PathVariable Long id) throws NotFoundException {
         return productService.findByIdFetchSchemas(id, AdminProductResponse.class)
                 .orElseThrow(() -> new NotFoundException("Product not found for id '" + id + "'"));
     }
 
+    @Operation(summary = "Update Product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Incorrect request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product doesn't exist", content = @Content)
+    })
     @PatchMapping(path = "/{id}", consumes = APPLICATION_JSON_VALUE)
     public AdminProductResponse update(
             @PathVariable Long id,
@@ -50,6 +82,14 @@ public class AdminProductController {
         return productService.findByIdFetchSchemas(id, AdminProductResponse.class).get();
     }
 
+    @Operation(summary = "Delete Product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "There are existing references to the Product", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthenticated", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product doesn't exist", content = @Content)
+    })
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) throws NotFoundException, ProductDeletionException {
         productService.delete(id);
