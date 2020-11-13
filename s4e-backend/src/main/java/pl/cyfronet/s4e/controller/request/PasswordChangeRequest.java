bf1fb264812d371474d126de21b2d618f0520c72
Jakub Sawicki <j.sawicki@cyfronet.pl>
2020-11-13 15:33:59 +0100

    [#797] Add password length validation when changing it

--- s4e-backend/src/main/java/pl/cyfronet/s4e/controller/request/PasswordChangeRequest.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/controller/request/PasswordChangeRequest.java
@@ -4,12 +4,15 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
 public class PasswordChangeRequest {
     @NotEmpty
     private String oldPassword;
+
     @NotEmpty
+    @Size(min=8)
     private String newPassword;
 }
--- s4e-backend/src/main/java/pl/cyfronet/s4e/controller/request/PasswordResetRequest.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/controller/request/PasswordResetRequest.java
@@ -4,12 +4,15 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
 public class PasswordResetRequest {
     @NotEmpty
+    @Size(min=8)
     private String password;
+
     @NotEmpty
     private String token;
 }
