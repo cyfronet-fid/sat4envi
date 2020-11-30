9db3eac315ca41a2e5a4d456b42a6bafb612f770
Michał Szostak <szostak.m.f@gmail.com>
2020-11-30 10:54:09 +0100

    [FIX] Persist HC / LargeFont settings

--- s4e-web/src/app/views/map-view/state/view-configuration/view-configuration.service.ts
+++ s4e-web/src/app/views/map-view/state/view-configuration/view-configuration.service.ts
@@ -1,19 +1,21 @@
 import { handleHttpRequest$ } from 'src/app/common/store.util';
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ViewConfigurationStore} from './view-configuration.store';
-import {ViewConfiguration} from './view-configuration.model';
+import {HC_LOCAL_STORAGE_KEY, LARGE_FONT_LOCAL_STORAGE_KEY, ViewConfiguration} from './view-configuration.model';
 import { finalize, map, shareReplay, tap } from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {IPageableResponse} from '../../../../state/pagable.model';
 import {ViewConfigurationQuery} from './view-configuration.query';
 import environment from 'src/environments/environment';
+import {LocalStorage} from '../../../../app.providers';
 
 @Injectable({providedIn: 'root'})
 export class ViewConfigurationService {
 
   constructor(private store: ViewConfigurationStore,
               private query: ViewConfigurationQuery,
+              @Inject(LocalStorage) private storage: Storage,
               private http: HttpClient) {
   }
 
@@ -57,10 +59,16 @@ export class ViewConfigurationService {
   }
 
   toggleHighContract() {
-    this.store.update(state => ({...state, highContrast: !state.highContrast}));
+    this.store.update(state => {
+      this.storage.setItem(HC_LOCAL_STORAGE_KEY, JSON.stringify(!state.highContrast))
+      return {...state, highContrast: !state.highContrast};
+    });
   }
 
   toggleLargeFont() {
-    this.store.update(state => ({...state, largeFont: !state.largeFont}));
+    this.store.update(state => {
+      this.storage.setItem(LARGE_FONT_LOCAL_STORAGE_KEY, JSON.stringify(!state.largeFont))
+      return {...state, largeFont: !state.largeFont}
+    });
   }
 }
--- s4e-web/src/app/views/map-view/state/view-configuration/view-configuration.store.spec.ts
+++ s4e-web/src/app/views/map-view/state/view-configuration/view-configuration.store.spec.ts
@@ -1,10 +1,20 @@
 import { ViewConfigurationStore } from './view-configuration.store';
+import {TestBed} from '@angular/core/testing';
+import {LocalStorageTestingProvider} from '../../../../app.configuration.spec';
+import {MapModule} from '../../map.module';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('ViewConfigurationStore', () => {
   let store: ViewConfigurationStore;
 
   beforeEach(() => {
-    store = new ViewConfigurationStore();
+    TestBed.configureTestingModule({
+      providers: [LocalStorageTestingProvider],
+      imports: [MapModule, HttpClientTestingModule, RouterTestingModule]
+    });
+
+    store = TestBed.get(ViewConfigurationStore);
   });
 
   it('should create an instance', () => {
