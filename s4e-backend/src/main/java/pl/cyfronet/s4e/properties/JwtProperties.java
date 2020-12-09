6ae86132cd13db80b32084e99e9b5cf0828789dd
Jakub Sawicki <j.sawicki@cyfronet.pl>
2020-12-09 12:17:55 +0100

    [#1006] Set domain for authorization cookie
    Depending on the property jwt.cookie.domain the cookie's domain is
    either fetched from request (in case the property is null or blank)
    and set to the value from the property otherwise.

--- s4e-backend/src/main/java/pl/cyfronet/s4e/properties/JwtProperties.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/properties/JwtProperties.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.time.Duration;
 
 @ConfigurationProperties("jwt")
@@ -29,10 +30,20 @@ public class JwtProperties {
     @NestedConfigurationProperty
     private Token token = new Token();
 
+    @NestedConfigurationProperty
+    private Cookie cookie = new Cookie();
+
     @Getter
     @Setter
     public static class Token {
         /// Half a day. In case of an 8h shift require a login every day.
         private Duration expirationTime = Duration.ofHours(12);
+    }
+
+    @Getter
+    @Setter
+    public static class Cookie {
+        @Pattern(regexp = "[^/:]+")
+        private String domain;
     }
 }
