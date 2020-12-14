c5f2c5bd4d05b41f02d815bcd8eafc5264217d19
Jakub Sawicki <j.sawicki@cyfronet.pl>
2020-12-14 17:17:50 +0100

    [#1022] Add polygon to SceneResponse
    Move mapping operations to a separate mapper.
    Add polygon to SceneResponse, its mapping and a test for correct
    operation.
    
    Promote a footprint mapping exception to WARN log call in
    AdminSceneMapper, as it's an erroneous behaviour which should be caught.
    Use the same log level in new SceneMapper footprint mapping.
    
    Correct the field order in SceneResponse to match with the Scene bean.

--- s4e-backend/src/main/java/pl/cyfronet/s4e/controller/response/SceneResponse.java
+++ s4e-backend/src/main/java/pl/cyfronet/s4e/controller/response/SceneResponse.java
@@ -1,28 +1,24 @@
 package pl.cyfronet.s4e.controller.response;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Builder;
 import lombok.Data;
+import org.locationtech.jts.geom.Geometry;
 import pl.cyfronet.s4e.bean.Legend;
 import pl.cyfronet.s4e.data.repository.projection.ProjectionWithId;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
-import java.util.Iterator;
 import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static pl.cyfronet.s4e.bean.Schema.SCENE_SCHEMA_ARTIFACTS_KEY;
 
 @Data
 @Builder
 public class SceneResponse {
     public interface Projection extends ProjectionWithId {
+        ProjectionWithId getProduct();
         String getSceneKey();
         LocalDateTime getTimestamp();
+        Geometry getFootprint();
         Legend getLegend();
         JsonNode getSceneContent();
         JsonNode getMetadataContent();
@@ -31,29 +27,9 @@ public class SceneResponse {
     private Long id;
     private Long productId;
     private String sceneKey;
+    private ZonedDateTime timestamp;
+    private String footprint;
     private Legend legend;
     private Set<String> artifacts;
     private JsonNode metadataContent;
-    private ZonedDateTime timestamp;
-
-    public static SceneResponse of(Long productId, Projection scene, Function<LocalDateTime, ZonedDateTime> timestampConverter) {
-        return SceneResponse.builder()
-                .id(scene.getId())
-                .productId(productId)
-                .sceneKey(scene.getSceneKey())
-                .timestamp(timestampConverter.apply(scene.getTimestamp()))
-                .legend(scene.getLegend())
-                .artifacts(getArtifactNames(scene))
-                .metadataContent(scene.getMetadataContent())
-                .build();
-    }
-
-    private static Set<String> getArtifactNames(Projection scene) {
-        ObjectNode artifacts = (ObjectNode) scene.getSceneContent().get(SCENE_SCHEMA_ARTIFACTS_KEY);
-        Iterator<String> artifactNamesIterator = artifacts.fieldNames();
-        return Stream.generate(() -> null)
-                .takeWhile(ignored -> artifactNamesIterator.hasNext())
-                .map(ignored -> artifactNamesIterator.next())
-                .collect(Collectors.toUnmodifiableSet());
-    }
 }
