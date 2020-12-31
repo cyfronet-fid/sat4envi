ce22fba266856f5bee9c5984e17d6bad00da43de
Andrzej Bacz <andrzej.bacz@gmail.com>
2020-12-31 15:30:55 +0100

    [#1003] css button and modal for mobile scene

--- s4e-web/src/app/views/map-view/timeline/mobile-scene-selector-modal/mobile-scene-selector-modal.component.spec.ts
+++ s4e-web/src/app/views/map-view/timeline/mobile-scene-selector-modal/mobile-scene-selector-modal.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MobileSceneSelectorModalComponent } from './mobile-scene-selector-modal.component';
+import {MapModule} from '../../map.module';
+import {LocalStorageTestingProvider, RemoteConfigurationTestingProvider} from '../../../../app.configuration.spec';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('MobileSceneSelectorModalComponent', () => {
   let component: MobileSceneSelectorModalComponent;
@@ -8,7 +11,8 @@ describe('MobileSceneSelectorModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MobileSceneSelectorModalComponent ]
+      imports: [MapModule, RouterTestingModule, HttpClientTestingModule],
+      providers: [RemoteConfigurationTestingProvider, LocalStorageTestingProvider]
     })
     .compileComponents();
   }));
