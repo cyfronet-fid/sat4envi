15394216fb787dc0096801ead531783e7e652242
Daniel Kryska <31220811+danielkryska@users.noreply.github.com>
2021-01-13 12:42:37 +0100

    Cypress performance and memory leak patch (#1073)
    - Run e2e in quiet mode without recording and photo making
    - Change e2e engine to chrome in headless mode to get more performance options
    - Set up browser one tab space and general shared space
    - Display failed tests logs in terminal instead of making images or videos of failed test case

--- s4e-web/cypress/plugins/index.js
+++ s4e-web/cypress/plugins/index.js
@@ -4,8 +4,44 @@ const fs = require('fs');
 module.exports = (on, config) => {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
   on('before:browser:launch', (browser = {}, launchOptions) => {
+    /**
+     * Filter console logs to the terminal, improve debugging process
+     */
+    require('cypress-log-to-output').install(on);
+
+    /**
+     * Chrome memory & security optimizations
+     */
+    if (browser.name === 'chrome') {
+      /**
+       * Disable same origin policy
+       */
+      launchOptions.args.push('--disable-web-security');
+
+      /**
+       * Enable tracing bugs inside iframes
+       */
+      launchOptions.args.push('--disable-site-isolation-trials');
+
+      /**
+       * Increase chrome tab memory limit
+       * By default 512MB on 32-bit systems or 1.4GB on 64-bit systems
+       */
+      launchOptions.args.push('--max_old_space_size=1024');
+
+      /**
+       * Increase chrome shared memory space from 64MB to unlimited (since Chrome 65)
+       */
+      launchOptions.args.push('--disable-dev-shm-usage');
+      return launchOptions;
+    }
   });
   on('task', {
+    /**
+     * Show full log in console of test on fail
+     */
+    failed: require('cypress-failed-log/src/failed')(),
+
     compareDownloadReport(now, timeout = config.defaultCommandTimeout) {
       now = now.replace(/:/g, '_');
       const downloadsFolder = require('downloads-folder');
