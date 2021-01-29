55dc31d123650d27aa352c6e8b98d6c3b746f53b
Marcelina <marcelinasiwecka@gmail.com>
2021-01-29 17:39:36 +0100

    Add mock for wms response in e2e tests
    Add mock for wms response in the tests which check addition of layers (e2e)

--- s4e-web/cypress/integration/settings/settings-institution-layer.spec.ts
+++ s4e-web/cypress/integration/settings/settings-institution-layer.spec.ts
@@ -11,6 +11,7 @@ import { SettingsNav } from "../../page-objects/settings/settings-navigation.po"
 before(() => {
   cy.fixture('users/zkAdmin.json').as('zkAdmin');
   cy.fixture('layer-capability.xml').as('geoserverResponse');
+  cy.fixture('image.png').as('image')
 });
 
 describe('Institution Layer', () => {
@@ -39,6 +40,16 @@ describe('Institution Layer', () => {
       response: this.geoserverResponse
     })
       .as('getCapabilities');
+
+    cy.route({
+      method: 'GET',
+      url: `https://localhost:4200/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=true&LAYERS=${layers}&*`,
+      headers: {
+          'Content-type': 'image/png'
+      },
+      status: 200,
+      response: this.image
+    });
 
     UserOptionsGoToSettings
       .gotoUserProfile();
